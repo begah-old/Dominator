e83bd7a282461bdf98701baae119ce5cc854d17c
Mathieu Roux <mathieu.roux222@gmail.com>
2016-07-15 14:55:10 +0200

    Finished IcoSphere Generator tool
    Finished the icosphere generator tool.
    Fixed name of files and changed obj files that are loaded, they are now
    only the upper part of the original sphere.

--- IcoSphere Generator/source/app.d
+++ IcoSphere Generator/source/app.d
@@ -7,27 +7,53 @@ import std.string;
 import std.algorithm;
 import std.array;
 
-import isosphere;
+import icosphere;
 
 import gl3n.linalg;
 
 int main() {
+	int subdivisionLevel = 3;
+	Start:
+	IsoSphere s = new IsoSphere(subdivisionLevel);
 
-	IsoSphere s = new IsoSphere(5);
+	File file = File("C:/Users/Begah/Documents/Dominator/IcoSphere Generator/assets/" ~ "out" ~ to!string(subdivisionLevel) ~ ".txt", "w");
 
-	File file = File("C:/Users/Mathieu Roux/Documents/D Workspace/Test/assets/" ~ "out.txt", "w");
-
-	file.write("Position = [ ");
 	auto pos = s.vertices.map!(a => a.position).array;
+
+	file.write("IcoSphere" ~ to!string(subdivisionLevel) ~ "_Positions = [ ");
 	foreach(i, p; pos) {
-		if((i + 1) % 5 == 0) { file.writeln("vec3(", p.x, ", ", p.y, ", ", p.z, "), "); file.write("\t"); }
+		if(i == pos.length - 1) file.write("vec3(", p.x, ", ", p.y, ", ", p.z, ")");
+		else if((i + 1) % 5 == 0) { file.writeln("vec3(", p.x, ", ", p.y, ", ", p.z, "), "); file.write("\t"); }
 		else file.write("vec3(", p.x, ", ", p.y, ", ", p.z, "), ");
 	}
+	file.writeln(" ];");
+	file.writeln();
 
-	file.write(" ];");
-	file.close();
-	auto norm = s.vertices.map!(a => a.normal);
-	auto textcoord = s.vertices.map!(a => a.textcoord);
+	auto norm = s.vertices.map!(a => a.normal).array;
+
+	file.write("IcoSphere" ~ to!string(subdivisionLevel) ~ "_Normals = [ ");
+	foreach(i, n; norm) {
+		if(i == norm.length - 1) file.write("vec3(", n.x, ", ", n.y, ", ", n.z, ")");
+		else if((i + 1) % 5 == 0) { file.writeln("vec3(", n.x, ", ", n.y, ", ", n.z, "), "); file.write("\t"); }
+		else file.write("vec3(", n.x, ", ", n.y, ", ", n.z, "), ");
+	}
+	file.writeln(" ];");
+	file.writeln();
+
+	auto textcoord = s.vertices.map!(a => a.textcoord).array;
+
+	file.write("IcoSphere" ~ to!string(subdivisionLevel) ~ "_TextCoords = [ ");
+	foreach(i, t; textcoord) {
+		if(i == textcoord.length - 1) file.write("vec2(", t.x, ", ", t.y / 2.0f + 0.5f, ")");
+		else if((i + 1) % 5 == 0) { file.writeln("vec2(", t.x, ", ", t.y / 2.0f + 0.5f, "), "); file.write("\t"); }
+		else file.write("vec2(", t.x, ", ", t.y, "), ");
+	}
+	file.writeln(" ];");
+
+	if(subdivisionLevel != 5) {
+		subdivisionLevel++;
+		goto Start;
+	}
 
 	return 0;
 }
